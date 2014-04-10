1423db4a2b373f593618f3eb672c203e9624bb3a
Sue Anne <sueanneteo@gmail.com>
2014-04-10 17:37:59 +0800

    amended gemfile and added views

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140410082619) do
+ActiveRecord::Schema.define(version: 20140410083601) do
+
+  create_table "comments", force: true do |t|
+    t.string   "body"
+    t.integer  "user_id"
+    t.integer  "space_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "spaces", force: true do |t|
     t.string   "name"
