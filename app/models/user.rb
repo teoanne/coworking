60d350d28f3e7f0e1afbb3241cec2d37421bcdc3
Sue Anne <sueanneteo@gmail.com>
2014-06-27 23:31:42 +0800

    shoulda matchers for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ActiveRecord::Base
-  has_many :spaces
-  has_many :comments
 
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
@@ -8,5 +6,8 @@ class User < ActiveRecord::Base
   validates_presence_of :country, :full_name
 
   has_secure_password validations: false #no password confirmation
+
+  has_many :spaces
+  has_many :comments
   
 end
