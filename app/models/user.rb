596e28d744befb45b179c204fb972662b80c89e8
Sue Anne <sueanneteo@gmail.com>
2014-07-08 02:00:08 +0800

    fixed rspec circular errors by removing --warnings from .rspec

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ActiveRecord::Base
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: { minimum: 8 }, on: :create
-  validates_presence_of :country, :full_name
+  validates_presence_of :country
+  validates :full_name, presence: true, uniqueness: true
 
   has_secure_password validations: false #no password confirmation
 
